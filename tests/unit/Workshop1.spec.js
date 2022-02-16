cb9a930236f21ada9a0f18e91253e853e5bdb044
Elisabeth Post <elisabeth.post@centauricorp.com>
2022-02-15 17:47:32 -0700

    change HelloWorld spec to workshop1 spec

--- tests/unit/Workshop1.spec.js
+++ tests/unit/Workshop1.spec.js
@@ -1,24 +1,24 @@
 import { shallowMount, config } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Workshop1 from "@/components/Workshop1.vue";
 import Vue from "vue";
 
 // removes vuetify warnings
 document.body.setAttribute("data-app", true);
 config.silent = true;
 Vue.config.silent = true;
-describe("HelloWorld.vue", () => {
+describe("Workshop1.vue", () => {
   test("The component renders", () => {
-    const wrapper = shallowMount(HelloWorld);
+    const wrapper = shallowMount(Workshop1);
     expect(wrapper.exists()).toBe(true);
   });
 
   test("We can access data variables", () => {
-    const wrapper = shallowMount(HelloWorld);
+    const wrapper = shallowMount(Workshop1);
     expect(wrapper.vm.testVariable).toBe(false);
   });
 
   test("We can access methods", () => {
-    const wrapper = shallowMount(HelloWorld);
+    const wrapper = shallowMount(Workshop1);
     wrapper.vm.testMethod();
     expect(wrapper.vm.testVariable).toBe(true);
   });
